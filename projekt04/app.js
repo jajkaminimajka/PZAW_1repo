3e3424ad97c648ec82d19dd43f6b9919c981a0eb
ententapl2 <maksikx08@onet.pl>
2026-05-21 11:06:44 +0000

    (SEC) choć działa to bcrypt jest przestarzały, spróbuj argon2

--- projekt04/app.js
+++ projekt04/app.js
@@ -1,7 +1,7 @@
 
 const express = require("express");
 const session = require("express-session");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcrypt"); // argon2
 
 const app = express();
 
